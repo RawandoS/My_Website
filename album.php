33a491fd62b2ae642fba8133fdde5829d5ce69e8
RawandoS <nicholasgardin07@gmail.com>
2025-06-11 17:31:20 +0200

    added get to album class

--- album.php
+++ album.php
@@ -9,9 +9,9 @@
         private array $labels;
         private array $trackNames;
         private array $trackTimes;
-        private array $albumTime;
+        private string $albumTime;
 
-        function __construct(int $id, array $title, array $artists, int $year, array $genres, array $styles, array $labels, array $trackNames, array $trackTimes, array $albumTime) {
+        function __construct(int $id, array $title, array $artists, int $year, array $genres, array $styles, array $labels, array $trackNames, array $trackTimes, string $albumTime) {
             $this->id = $id;
             $this->title = $title;
             $this->artists = $artists;
@@ -22,6 +22,36 @@
             $this->trackNames = $trackNames;
             $this->trackTimes = $trackTimes;
             $this->albumTime = $albumTime;
+        }
+        public function getId(): int {
+            return $this->id;
+        }
+        public function getTitle(): array {
+            return $this->title;
+        }
+        public function getArtists(): array {
+            return $this->artists;
+        }
+        public function getYear(): int {
+            return $this->year;
+        }
+        public function getGenres(): array {
+            return $this->genres;
+        }
+        public function getStyles(): array {
+            return $this->styles;
+        }
+        public function getLabels(): array {
+            return $this->labels;
+        }
+        public function getTrackNames(): array {
+            return $this->trackNames;
+        }
+        public function getTrackTimes(): array {
+            return $this->trackTimes;
+        }
+        public function getAlbumTime(): string {
+            return $this->albumTime;
         }
     }
 ?>
