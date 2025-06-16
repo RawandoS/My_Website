55a4de1ae2b597533f836cfcfca35b5b9ff9ac3c
RawandoS <nicholasgardin07@gmail.com>
2025-06-16 08:36:28 +0200

    CSS on admin.php

--- admin.php
+++ admin.php
@@ -16,7 +16,7 @@
     <body>
         <header>
             <div class="hub">
-                <h2>DATABSE CONTROL</h2>
+                <h2>DATABASE CONTROL</h2>
             </div>
             <nav class="navbar">
                 <div class="leftNav">
@@ -33,52 +33,48 @@
             </nav>
         </header>
         <div class="centralGrid">
-            <div class="leftgrid">
-                <center>
-                    <form class="databaseForm" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-                        <label>Album to add (Artist + albumName):</label><br>
-                        <input type="text" name="albumName"><br>
-                        <input type="submit" name="submit" value="Add to Database">
-                    </form>
-                    <?php
-                        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["albumName"])){
-                            
-                            $albumName = filter_input(INPUT_POST,"albumName",
-                                            FILTER_SANITIZE_SPECIAL_CHARS);
-                            if (empty($albumName)) {
-                                echo '<script>alert("Add a real name")</script>';
-                                exit();
-                            }
-                            $check = addAlbumToDatabase($albumName);
-                            if (!$check) {
-                                echo "Album not added";
-                            }
-                            header("Location: " . $_SERVER['PHP_SELF']);
+            <div class="leftGrid">
+                <form class="databaseForm" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                    <label id="addAlbum">Album to add (Artist + albumName):</label><br>
+                    <input type="text" name="albumName"><br>
+                    <input type="submit" name="submit" value="Add to Database">
+                </form>
+                <?php
+                    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["albumName"])){
+                        
+                        $albumName = filter_input(INPUT_POST,"albumName",
+                                        FILTER_SANITIZE_SPECIAL_CHARS);
+                        if (empty($albumName)) {
+                            echo '<script>alert("Add a real name")</script>';
                             exit();
                         }
-                    ?>
-                </center>
+                        $check = addAlbumToDatabase($albumName);
+                        if (!$check) {
+                            echo "Album not added";
+                        }
+                        header("Location: " . $_SERVER['PHP_SELF']);
+                        exit();
+                    }
+                ?>
             </div>
             <div class="rightGrid">
-                <center>
-                    <form class="databaseForm" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-                        <label>Album to add:</label><br>
-                        <input type="text" name="albumID"><br>
-                        <input type="submit" name="submit" value="Search from Database">
-                    </form>
-                    <?php
-                        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["albumID"])){
-                            $albumID = filter_input(INPUT_POST,"albumID",
-                                            FILTER_SANITIZE_SPECIAL_CHARS);
-                            if (empty($albumID)) {
-                                echo '<script>alert("Add a real name")</script>';
-                                exit();
-                            }
-                            printAlbumFromDatabase($albumID);
-                            $_POST ["albumID"] = "";
+                <form class="databaseForm" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                    <label id="insert">Insert album ID:</label><br>
+                    <input type="text" name="albumID"><br>
+                    <input type="submit" name="submit" value="Search from Database">
+                </form>
+                <?php
+                    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["albumID"])){
+                        $albumID = filter_input(INPUT_POST,"albumID",
+                                        FILTER_SANITIZE_SPECIAL_CHARS);
+                        if (empty($albumID)) {
+                            echo '<script>alert("Add a real name")</script>';
+                            exit();
                         }
-                    ?>
-                </center>
+                        printAlbumFromDatabase($albumID);
+                        $_POST ["albumID"] = "";
+                    }
+                ?>
             </div>
         </div>
         <footer>
