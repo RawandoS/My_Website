c21780685f73db081a2e93492cb0f513eff6f85f
RawandoS <nicholasgardin07@gmail.com>
2025-06-25 17:31:55 +0200

    minor css fix

--- app/views/coverShow.view.php
+++ app/views/coverShow.view.php
@@ -90,7 +90,7 @@
             $id = 1;
         }
     ?>
-    <bod id="<?php echo $id; ?>>
+    <body>
         <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
             <a href="" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
                 <svg class="bi me-2" width="40" height="32" aria-hidden="true"><use xlink:href="#bootstrap"></use></svg> <span class="fs-4">My Website</span>
